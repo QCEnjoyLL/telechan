1327645bb7e548126dcc9dbd7ff24a397b7a55d7
Orange <klts1228@163.com>
2021-05-30 21:21:23 +0800

    URL

--- src/commands/sendkey.ts
+++ src/commands/sendkey.ts
@@ -1,6 +1,7 @@
 import md5 from 'md5';
 const VERCEL_URL = process.env.VERCEL_URL;
 const TCKEY = process.env.TCKEY;
+const OWN_URL = process.env.OWN_URL;
 
 const replyToMessage = (ctx: any, messageId: string, string: string) =>
   ctx.reply(string, {
@@ -13,11 +14,11 @@ const sendkey = () => (ctx: any) => {
   const userName = ctx.from.last_name ? `${ctx.from.first_name} ${ctx.from.last_name}` : ctx.from.first_name;
   const sendkey = ctx.from.id +'T'+md5(TCKEY+ctx.from.id);
 
-  const site_url = String(VERCEL_URL);
+  const site_url = String(OWN_URL);
 
   replyToMessage(ctx, messageId, `${userName} , Your sendkey is 🔑 ${sendkey} \n 
   🚀 Use follow url to send message : \n 
-  ${VERCEL_URL}/api/send?sendkey=<sendkey>&text=<text>`);
+  ${site_url}/api/send?sendkey=<sendkey>&text=<text>`);
   // replyToMessage(ctx, messageId, `Hello, ${userName} (user_id: ${ctx.from.id})! \n Your Message id is: ${messageId}`);
 };
 
